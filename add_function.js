ec3ca0774763db33ab2da59a31709a76b33aeb73
trideviataya <olgachichkanova@gmail.com>
2019-05-29 10:29:10 +0800

    default value added placeholder added

--- add_function.js
+++ add_function.js
@@ -4,9 +4,12 @@ function add() {
     const x = Number(document.getElementById('first_number').value);
     const y = Number(document.getElementById('second_number').value);
 
+    document.getElementById('first_number').defaultValue = 0.1;
+    document.getElementById('second_number').defaultValue = 0.2;
+
     document.getElementById('result').innerHTML = x+y;
     return false;
 }
 
 document.getElementById('add_button').addEventListener('click', add);
-//to do: values by default
+
